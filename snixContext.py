3e762971d7d0e133a2712b0f5306a84325c939e2
nishant.kakar <nishant.kakar@mulesoft.com>
2017-03-03 16:01:22 -0800

    Bug fix in snixContext to remove "config"

--- snixContext.py
+++ snixContext.py
@@ -7,6 +7,7 @@ from jsonschema import Draft4Validator
 from item import Item
 from repo import Repo
 from script import Script
+import ConfigParser
 
 
 class snixContext:
@@ -23,7 +24,6 @@ class snixContext:
         if not os.path.isfile(_file):
             snixCore.abort("%s is not a valid file path!" % _file)
         self._file = _file
-        self._manifest_config = {}
         self._manifest_items = []
         self._manifest_repos = []
         self._manifest_custom_scripts = []
@@ -31,15 +31,14 @@ class snixContext:
     # TODO navigate all includes.
     # make sure it doesn't have cycles.
     def _construct(self):
+        snixConf = os.path.join(os.environ["HOME"],".snix","snix.conf")
         with open(self._file, 'r') as candidate:
             _data = json.load(candidate)
-        with open(os.path.join(_data['config']['snix_root'],'_snix','schema.json'), 'r') as schema:
-            _schema = json.load(schema)
-            Draft4Validator(_schema).validate(_data)
-        self._manifest_config = _data['config']
-
+            parser = ConfigParser.ConfigParser()
+            parser.read(snixConf)
+            snixHome = parser.get("config", "snix.home")
         if 'includes' in _data:
-            self._collect_includes(_data)
+            self._collect_includes(_data,snixHome)
         if 'items' in _data:
             map(lambda i:self._manifest_items.append(i), _data['items'])
         if 'repos' in _data:
@@ -48,28 +47,25 @@ class snixContext:
             map(lambda i:self._manifest_custom_scripts.append(i), _data['customScripts'])
 
 
-    def _collect_includes(self, _data):
-        _root = _data['config']['snix_root']
+    def _collect_includes(self, _data,_snixHome):
+        
         for include in _data['includes']:
             _repo = include['upstreamRepo']
             _dir = _repo.split('/')[-1].split('.')[0]
             if not os.path.exists(_dir):
                 includeContext={}
-                includeContext['snix_root']= _root
+                includeContext['snix_root']= _snixHome
                 includeContext['repo_location']= _repo
                 Repo(includeContext).clone()
             grp_dir = include['pathRelativeToGroupManifestDir']
-            _include_file = os.path.join(_root, _dir, grp_dir,grp_dir+'.snix')
-        self._collect_from_file(_include_file)
+            _include_file = os.path.join(_snixHome, _dir, grp_dir,grp_dir+'.snix')
+        self._collect_from_file(_include_file,_snixHome)
 
-    def _collect_from_file(self, _include_file):
+    def _collect_from_file(self, _include_file, _snixHome):
         with open(_include_file, 'r') as candidate:
             _data = json.load(candidate)
-        with open(os.path.join(self._manifest_config['snix_root'],'_snix','schema.json'), 'r') as schema:
-            _schema = json.load(schema)
-            Draft4Validator(_schema).validate(_data)
         if 'include' in _data:
-            self._collect_includes(_data)
+            self._collect_includes(_data, _snixHome)
         if 'items' in _data:
             map(lambda i:self._manifest_items.append(i), _data['items'])
         if 'repos' in _data:
@@ -81,27 +77,24 @@ class snixContext:
         items = [''.join("{0} via {1}".format(item['names'], item['via'])) for item in iter(self._manifest_items)]
         return '\n'.join(["\nItems to install: {0}".format(items),
                           "Repositories to clone:{0}".format(json.dumps(self._manifest_repos, indent=2)),
-                          "Custom scripts to execute:{0}".format(json.dumps(self._manifest_custom_scripts, indent=2)),
-                          "Configuration:{0}".format(json.dumps(self._manifest_config, indent=2))])
+                          "Custom scripts to execute:{0}".format(json.dumps(self._manifest_custom_scripts, indent=2))])
 
     # TODO: make this take in a filter.
     def get_items(self):
-        def _build_item(name, via, config):
+        def _build_item(name, via):
             item_context = {'name': name, 'via': via}
-            item_context.update(config)
             return Item(item_context)
 
         all_items = []
         for item in self._manifest_items:
             for name in item['names']:
-                all_items.append(_build_item(name, item['via'], self._manifest_config))
+                all_items.append(_build_item(name, item['via']))
         return all_items
 
     def get_repos(self):
         all_repos=[]
         for repo in self._manifest_repos:
             repo_context = {'repo_location': repo}
-            repo_context.update(self._manifest_config)
             all_repos.append(Repo(repo_context))
         return all_repos
 
@@ -109,7 +102,6 @@ class snixContext:
         all_scripts=[]
         for script in self._manifest_custom_scripts:
             custom_script_context = {'script_location': script}
-            custom_script_context.update(self._manifest_config)
             all_scripts.append(Script(custom_script_context))
         return all_scripts
 
